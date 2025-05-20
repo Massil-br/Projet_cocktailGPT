b0d3e9f1be49f75d47800bb94659090958cfc306
Massil <160241880+Massil-br@users.noreply.github.com>
2025-05-20 23:27:56 +0200

    added real cocktails to the database

--- src/routes/skins.js
+++ src/routes/skins.js
@@ -1,15 +1,17 @@
-    import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 
 export async function getFortniteSkins(req, res) {
   try {
     const response = await fetch('https://fortnite-api.com/v2/cosmetics/br');
     const data = await response.json();
     const skins = data.data
-        .filter(item => item.type.value === 'outfit' && item.name && item.images?.icon) // 👈 Ajouté
-        .map(skin => ({
-            name: skin.name,
-            imageUrl: skin.images.icon
-        }));
+      .filter(item => item.type.value === 'outfit' && item.name && item.images?.icon)
+      .map(skin => ({
+        name: skin.name,
+        imageUrl: skin.images.icon
+      }))
+      .sort((a, b) => a.name.localeCompare(b.name)); // 👈 Tri alphabétique ici
+
     res.json(skins);
   } catch (error) {
     res.status(500).json({ error: 'Erreur récupération skins Fortnite' });
